933d20ffd9e4a57ead882afde430ad2dcd2c6fad
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-01-13 21:09:54 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Friendly", True );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Brown", "Mellow", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 17, "F", "Black", "Timid", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "Blonde", "Friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "Cinnamon", "Protective", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "Blonde", "Timid", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 20, "F", "White", "Protective", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "F", "Brown", "Mellow", False);
